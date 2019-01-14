135b41a1126179b9df99189cfd0f42a640306010
bobnearents <bobnearents@yahoo.com>
2019-01-14 11:40:01 -0800

    fixed bugs

--- set1.js
+++ set1.js
@@ -9,22 +9,22 @@ function createGreeting(name, age) {
 
     const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
-};
+}
 
 function getYearOfBirth(age) {
     if (age<0) {
         throw new Error("Age can not be negative");
     }
     return 2019 - age;
-};
+}
 
 try {
-    const greeting = createGreeting();
+    const greeting = createGreeting('bob',26);
+    console.log(greeting);
 } catch(e) {
     console.log(e);
-};
+}
 
-console.log(greeting);
 
 
 
