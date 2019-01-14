efd180cd6fc173bdb2fc9ab59a0380a3a1ac23b5
bobnearents <bobnearents@yahoo.com>
2019-01-14 11:22:42 -0800

    added year of birth function

--- set1.js
+++ set1.js
@@ -1,7 +1,12 @@
 function createGreeting(name, age) {
-    const yearOfBirth = 2019-age;
+    const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}`;
 };
+
+function getYearOfBirth(age) {
+    
+    return 2019 - age;
+}
 
 const greeting = createGreeting();
 
